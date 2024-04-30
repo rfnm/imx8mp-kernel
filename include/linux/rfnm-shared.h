cc70abfb35bf64b936ab99ac3cffb962be009869
Davide Cavion <davide@rfnm.io>
2024-04-30 20:21:49 +0200

    Shared header changes

--- include/linux/rfnm-shared.h
+++ include/linux/rfnm-shared.h
@@ -1,10 +1,15 @@
 #ifndef INCLUDE_LINUX_RFNM_SHARED_H_
 #define INCLUDE_LINUX_RFNM_SHARED_H_
 
+
+
 #define RFNM_DAUGHTERBOARD_BREAKOUT (1)
 #define RFNM_DAUGHTERBOARD_GRANITA (2)
 #define RFNM_DAUGHTERBOARD_LIME (3)
 #define RFNM_MOTHERBOARD_BLUE (4)
+
+#define RFNM_BOARD_ID_TO_USER_READABLE_NAME (char[][30]){"", "Breakout", "Granita", "Lime", "Motherboard"}
+
 
 #define RFNM_SLOT_PRIMARY (0)
 #define RFNM_SLOT_SECONDARY (1)
@@ -29,15 +34,15 @@
 #define GP_OUT_7 ( 7 )
 
 #define RFNM_ADC_MAP (int[]){0x2, 0x4, 0x1, 0x3}
-
-
+	
+#define RFNM_DAC_BUFCNT 0x4000
 
 
 #define RFNM_TX (0)
 #define RFNM_RX (1)
 
 
-#define MHZ_TO_HZ * 1000 * 1000ul
+#define MHZ_TO_HZ(MHz) (MHz * 1000 * 1000ul)
 #define HZ_TO_MHZ(Hz) (Hz / (1000ul * 1000ul))
 #define HZ_TO_KHZ(Hz) (Hz / 1000ul)
 
@@ -113,7 +118,7 @@ struct __attribute__((__packed__)) rfnm_bootconfig {
 
 
 
-
+#include <linux/rfnm-api.h>
 
 
 
@@ -122,137 +127,6 @@ struct __attribute__((__packed__)) rfnm_bootconfig {
 //#define RFNM_MAX_TRX_CH_CNT (8)
 
 
-enum rfnm_ch_data_type {
-	RFNM_CH_DATA_TYPE_COMPLEX,
-	RFNM_CH_DATA_TYPE_REAL
-};
-
-enum rfnm_agc_type {
-    RFNM_AGC_OFF,
-    RFNM_AGC_DEFAULT
-};
-
-enum rfnm_bias_tee {
-    RFNM_BIAS_TEE_OFF,
-    RFNM_BIAS_TEE_ON
-};
-
-enum rfnm_rf_path {
-	RFNM_PATH_SMA_A,
-	RFNM_PATH_SMA_B,
-	RFNM_PATH_SMA_C,
-	RFNM_PATH_SMA_D,
-	RFNM_PATH_SMA_E,
-	RFNM_PATH_SMA_F,
-	RFNM_PATH_SMA_G,
-	RFNM_PATH_SMA_H,
-    RFNM_PATH_EMBED_ANT,
-    RFNM_PATH_LOOPBACK,
-	RFNM_PATH_NULL
-};
-
-enum rfnm_ch_enable {
-    RFNM_CH_OFF,
-    RFNM_CH_ON,
-	RFNM_CH_ON_TDD
-};
-
-
-
-
-RFNM_PACKED_STRUCT(
-    struct rfnm_api_tx_ch {
-		int8_t abs_id;
-		int8_t dgb_ch_id;
-		int8_t dgb_id;
-		int8_t dac_id;
-        int64_t freq_min;
-        int64_t freq_max;
-        int64_t freq;
-		int16_t iq_lpf_bw;
-        int16_t samp_freq_div_m;
-        int16_t samp_freq_div_n;
-        int8_t avail;
-        int8_t power;
-		enum rfnm_ch_enable enable;
-		enum rfnm_bias_tee bias_tee;
-		enum rfnm_rf_path path;
-		enum rfnm_rf_path path_possible[10];		
-		enum rfnm_ch_data_type data_type;
-    }
-);
-
-RFNM_PACKED_STRUCT(
-    struct rfnm_api_rx_ch {
-		int8_t abs_id;
-		int8_t dgb_ch_id;
-		int8_t dgb_id;
-		int8_t adc_id;
-        int64_t freq_min;
-        int64_t freq_max;
-        int64_t freq;
-		int16_t iq_lpf_bw;
-        int16_t samp_freq_div_m;
-        int16_t samp_freq_div_n;
-        int8_t avail;
-        int8_t gain;
-        enum rfnm_ch_enable enable;
-		enum rfnm_agc_type agc;
-		enum rfnm_bias_tee bias_tee;
-        enum rfnm_rf_path path;
-		enum rfnm_rf_path path_possible[10];		
-		enum rfnm_ch_data_type data_type;
-    }
-);
-
-RFNM_PACKED_STRUCT(
-	struct rfnm_dev_hwinfo_bit {
-		uint8_t board_id;
-		uint8_t board_revision_id;
-		uint8_t serial_number[9];
-		char user_readable_name[30];
-		uint8_t mac_addr[6];
-		uint8_t tx_ch_cnt;
-		uint8_t rx_ch_cnt;
-		int16_t temperature;
-	}
-);
-
-/*RFNM_PACKED_STRUCT(
-	struct rfnm_dev_hwinfo_clockgen {
-		uint32_t dcs_clk;
-	}
-)*/
-
-RFNM_PACKED_STRUCT(
-	struct rfnm_dev_hwinfo {
-		struct rfnm_dev_hwinfo_bit motherboard;
-		struct rfnm_dev_hwinfo_bit daughterboard[2];
-	//	struct rfnm_dev_hwinfo_clockgen clock;
-	}
-);
-
-RFNM_PACKED_STRUCT(
-	struct rfnm_dev_tx_ch_list {
-		int cnt;
-		struct rfnm_api_tx_ch tx_ch[8];
-	}
-);
-
-RFNM_PACKED_STRUCT(
-	struct rfnm_dev_rx_ch_list {
-		int cnt;
-		struct rfnm_api_rx_ch rx_ch[8];
-	}
-);
-
-enum rfnm_control_ep {
-    RFNM_GET_DEV_HWINFO = 0xf00,
-    RFNM_GET_TX_CH_LIST,
-	RFNM_SET_TX_CH_LIST,
-	RFNM_GET_RX_CH_LIST,
-	RFNM_SET_RX_CH_LIST
-};
 
 RFNM_PACKED_STRUCT(
 	struct fe_s {
@@ -293,10 +167,8 @@ struct rfnm_dgb {
 	uint8_t dac_ifs;
 	uint8_t adc_iqswap[2];
 	uint8_t dac_iqswap[2];
-	
-
-
 };
+
 RFNM_PACKED_STRUCT(
 	struct rfnm_m7_dgb {
 		struct fe_s fe;
@@ -317,20 +189,14 @@ void rfnm_dgb_en_tdd(struct rfnm_dgb *dgb_dt, struct rfnm_api_tx_ch * tx_ch, str
 void rfnm_populate_dev_hwinfo(struct rfnm_dev_hwinfo *r_hwinfo);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void rfnm_populate_dev_tx_chlist(struct rfnm_dev_tx_ch_list * r_chlist);
+void rfnm_populate_dev_rx_chlist(struct rfnm_dev_rx_ch_list * r_chlist);
+void rfnm_apply_dev_tx_chlist(struct rfnm_dev_tx_ch_list * r_chlist);
+void rfnm_apply_dev_rx_chlist(struct rfnm_dev_rx_ch_list * r_chlist);
+void rfnm_populate_dev_set_res(struct rfnm_dev_get_set_result * r_res);
+int rfnm_la9310_stream(uint8_t tx, uint8_t *rx);
+int rfnm_la9310_stream(uint8_t tx, uint8_t *rx);
+void rfnm_populate_dev_status(struct rfnm_dev_status * r_stat);
+void rfnm_restart_sm(int hard);
 
 #endif
